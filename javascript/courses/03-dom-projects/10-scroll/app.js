c382ee53786f7591a0672cc8628fc1f25c4365d9
Thomas ROBERT <thomas_3004@hotmail.fr>
2022-11-03 11:03:46 +0100

    Add postion and media queries courses + implement nav toggle in scroll project

---
+++ javascript/courses/03-dom-projects/10-scroll/app.js
@@ -0,0 +1,19 @@
+// ********** défini la date ************
+const date = document.querySelector('.date');
+date.textContent = new Date().getFullYear();
+
+// ********** ouvre/ferme les liens ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function () {
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});

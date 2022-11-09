33cd6b9b5327b9c8debc182f303bd67726a42fc6
Thomas ROBERT <thomas_3004@hotmail.fr>
2022-11-09 10:38:21 +0100

    Init grocery bud project

---
+++ javascript/courses/03-dom-projects/14-grocery-bud/final/app.js
@@ -0,0 +1,45 @@
+// ****** SELECTIONNER LES ELEMENTS HTML **********
+
+// option d'édition
+
+// ****** EVENT LISTENERS **********
+// soumettre le formulaire
+
+// supprimer tous les items
+
+// charger les items (DOMContentLoaded)
+
+// ****** FONCTIONS **********
+// ajouter un item
+function addItem(e) {}
+
+// afficher l'alerte
+function displayAlert(text, action) {
+  // supprimer l'alerte
+}
+
+function setBackToDefault() {}
+
+// supprimer tous les items
+function clearItems() {}
+
+// supprimer un item
+function deleteItem(e) {}
+
+// éditer un item
+function editItem(e) {}
+
+// ****** LOCAL STORAGE **********
+function addToLocalStorage(id, value) {}
+
+function removeFromLocalStorage(id) {}
+
+function editLocalStorage(id, value) {}
+
+function getLocalStorage() {}
+
+// ****** METTRE EN PLACE LES ITEMS **********
+// récupérer la liste dans le localStorage ou en créer une vide
+function setupItems() {}
+
+function createListItem(id, value) {}

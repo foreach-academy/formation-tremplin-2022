5934995059552bfc7520bac8ff3979701e874ae1
Thomas ROBERT <thomas_3004@hotmail.fr>
2022-11-22 11:16:15 +0100

    Create Gallery class and add open/close modal + setMainImage methods

--- javascript/courses/05-oop-projects/02-gallery/app-class.js
+++ javascript/courses/05-oop-projects/02-gallery/app-class.js
@@ -1,4 +1,4 @@
-const getElement = (selector, list) => {
+const getElement = (selector, list = false) => {
   const el = list
     ? [...document.querySelectorAll(selector)]
     : document.querySelector(selector);
@@ -14,4 +14,56 @@ const getElement = (selector, list) => {
   );
 };
 
-console.log('hello');
+class Gallery {
+  constructor(element) {
+    this.container = element;
+    this.list = [...element.querySelectorAll('.img')];
+    // target
+    this.modal = getElement('.modal');
+    this.modalImg = getElement('.main-img');
+    this.imageName = getElement('.image-name');
+    this.modalImages = getElement('.modal-images');
+    this.closeBtn = getElement('.close-btn');
+    this.prevBtn = getElement('.prev-btn');
+    this.nextBtn = getElement('.next-btn');
+
+    // bind
+    this.closeModal = this.closeModal.bind(this);
+
+    // event
+    this.container.addEventListener(
+      'click',
+      function (e) {
+        if (e.target.classList.contains('img')) {
+          this.openModal(e.target, this.list);
+        }
+      }.bind(this)
+    );
+  }
+
+  openModal(selectedImage, list) {
+    this.setMainImage(selectedImage);
+    this.closeBtn.addEventListener('click', this.closeModal);
+    this.modalImages.innerHTML = list
+      .map(function (image) {
+        return `<img src=${
+          image.src
+        } title="${image.title}" data-id="${image.dataset.id}" class="modal-img ${selectedImage.dataset.id === image.dataset.id && 'selected'}" />`;
+      })
+      .join('');
+    this.modal.classList.add('open');
+  }
+
+  closeModal() {
+    this.modal.classList.remove('open');
+    this.closeBtn.removeEventListener('click', this.closeModal);
+  }
+
+  setMainImage(selectedImage) {
+    this.modalImg.src = selectedImage.src;
+    this.imageName.textContent = selectedImage.title;
+  }
+}
+
+const nature = new Gallery(getElement('.nature', false));
+const city = new Gallery(getElement('.city', false));

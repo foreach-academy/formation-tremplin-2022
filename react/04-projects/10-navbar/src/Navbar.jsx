76e642ee5ceaab6a55201b63c539feeaebc2a292
Thomas ROBERT <thomas_3004@hotmail.fr>
2022-12-16 15:44:41 +0100

    Complete navbar project

--- react/04-projects/10-navbar/src/Navbar.jsx
+++ react/04-projects/10-navbar/src/Navbar.jsx
@@ -4,17 +4,33 @@ import { links, social } from './data';
 import logo from './logo.png';
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = 0;
+    }
+  }, [showLinks]);
+
   return (
     <nav>
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} alt='logo' className='logo' />
-          <button className='nav-toggle'>
+          <button
+            className='nav-toggle'
+            onClick={() => setShowLinks(!showLinks)}>
             <FaBars />
           </button>
         </div>
-        <div className='links-container'>
-          <ul className='links'>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
 

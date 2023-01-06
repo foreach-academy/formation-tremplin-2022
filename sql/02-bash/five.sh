5408df5f92485f1465f9fcad3353079558830f38
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-01-06 15:17:30 +0100

    Add five project and refactor questionnaire project

---
+++ sql/02-bash/five.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Programme pour exécuter mes quatre autres programmes
+
+./questionnaire.sh
+./countdown.sh 10
+./bingo.sh
+./fortune.sh
--- sql/02-bash/questionnaire.sh
+++ sql/02-bash/questionnaire.sh
@@ -3,17 +3,16 @@
 echo -e "\n~~ Questionnaire ~~\n"
 
 QUESTION1="Comment t'appelles-tu ?"
+QUESTION2="\nD'où venez vous ?"
+QUESTION2="\nQuel est votre site Web de codage préféré ?"
+
 echo $QUESTION1
 read NAME
-echo hello $NAME
 
-QUESTION2="D'où venez vous ?"
-echo $QUESTION2
+echo -e $QUESTION2
 read LOCATION
-echo Bonjour $NAME de $LOCATION
 
-QUESTION2="Quel est votre site Web de codage préféré ?"
-echo $QUESTION2
+echo -e $QUESTION2
 read WEBSITE
 
 echo -e "\nBonjour $NAME de $LOCATION. J'ai appris que votre site Web de codage préféré est $WEBSITE !"
